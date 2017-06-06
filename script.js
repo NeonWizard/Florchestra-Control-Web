15bef69a0e53ea8c5114efe214dc47414dc619cf
NeonWizard <averagewizard13@gmail.com>
2017-06-06 14:49:47 -0600

    Buttons are all now fully functional

--- script.js
+++ script.js
@@ -13,31 +13,116 @@ function startEngine() {
 	if (engineOn) return;
 	engineOn = true;
 
-	for (let element in elements) {
-		elements[element].disabled = false;
-		elements[element].className = "";
-	}
 	elements["start"].disabled = true; elements["start"].className = "disabled";
-	elements["switch"].disabled = true;
-	// document.querySelector("#bigrange-switch .slider").style.backgroundColor = "rgb(20, 20, 20)";
-	document.querySelector("#bigrange-label").style.color = "gray";
+
+	fetch(new Request(`${apiURL}/startEngine`), {
+		method: "POST",
+		credentials: "include",
+		body: JSON.stringify({
+			bigrange: elements["switch"].checked
+		}),
+		headers: new Headers({"Content-Type": "application/json"})
+	}).then((response) => {
+		for (let element in elements) {
+			elements[element].disabled = false;
+			elements[element].className = "";
+		}
+		elements["start"].disabled = true; elements["start"].className = "disabled";
+		elements["switch"].disabled = true;
+		document.querySelector("#bigrange-label").style.color = "gray";
+		document.querySelector("#bigrange-switch .slider").style.cursor = "default";
+
+		if (!response.ok) throw response;
+		return response;
+	}).then((response) => {
+		return response.text();
+	}).then((goodText) => {
+		console.log("Successfully started engine.");
+	}).catch((response) => {
+		response.text().then((errorText) => {
+			console.log(errorText);
+		});
+	});
 }
 
 function stopEngine() {
 	if (!engineOn) return;
 	engineOn = false;
 
-	for (let element in elements) {
-		elements[element].disabled = true;
-		elements[element].className = "disabled";
-	}
-	elements["start"].disabled = false; elements["start"].className = "";
-	elements["switch"].disabled = false;
-	// document.querySelector("#bigrange-switch .slider").style.backgroundColor = "#bbb";
-	document.querySelector("#bigrange-label").style.color = "white";
+	fetch(new Request(`${apiURL}/stopEngine`), {
+		method: "POST",
+		credentials: "include"
+	}).then((response) => {
+		for (let element in elements) {
+			elements[element].disabled = true;
+			elements[element].className = "disabled";
+		}
+		elements["start"].disabled = false; elements["start"].className = "";
+		elements["switch"].disabled = false;
+		document.querySelector("#bigrange-label").style.color = "white";
+		document.querySelector("#bigrange-switch .slider").style.cursor = "pointer";
+
+		if (!response.ok) throw response;
+		return response;
+	}).then((response) => {
+		return response.text();
+	}).then((goodText) => {
+		console.log("Successfully stopped engine.");
+	}).catch((response) => {
+		response.text().then((errorText) => {
+			console.log(errorText);
+		});
+	});
+}
+
+function playSong() {
+	if (!engineOn) return;
+
+	fetch(new Request(`${apiURL}/play`), {
+		method: "POST",
+		credentials: "include",
+		body: JSON.stringify({
+			song: elements["dropdown"].value,
+			bigrange: elements["switch"].checked
+		})
+	}).then((response) => {
+		if (!response.ok) throw response;
+		return response;
+	}).then((response) => {
+		return response.text();
+	}).then((goodText) => {
+		console.log("Playing song.");
+	}).catch((response) => {
+		response.text().then((errorText) => {
+			console.log(errorText);
+		});
+	});
 }
 
 
+// If engine is already on, then set button states to proper form
+fetch(new Request(`${apiURL}/engineState`), {
+	method: "GET",
+	credentials: "include"
+}).then((response) => {
+	return response.json();
+}).then((engineState) => {
+	if (engineState.active) {
+		engineOn = true;
+
+		for (let element in elements) {
+			elements[element].disabled = false;
+			elements[element].className = "";
+		}
+		elements["start"].disabled = true; elements["start"].className = "disabled";
+		elements["switch"].checked = engineState.bigRange;
+		elements["switch"].disabled = true;
+		document.querySelector("#bigrange-label").style.color = "gray";
+		document.querySelector("#bigrange-switch .slider").style.cursor = "default";
+	}
+});
+
+// Populate dropdown list with song choices
 fetch(new Request(`${apiURL}/songs`), {
 	method: "GET",
 	credentials: "include"
